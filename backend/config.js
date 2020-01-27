962f0ee2b054fefac05648baea20ddbb49b8cef1
unknown <tatu.riihimaki@gmail.com>
2020-01-27 19:40:27 +0200

    Added readme

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     FILEPATH: process.env.FILEPATH || "./mockdata.real",
     DIST_PATH: process.env.DIST_PATH || "../frontend/dist",
-    ALLOWED_ORIGINS: ["http://localhost:8080/", "https://pkg-viewer.herokuapp.com"]
+    ALLOWED_ORIGINS: ["http://localhost:8080/", "https://pkg-viewer.herokuapp.com/"]
 }
