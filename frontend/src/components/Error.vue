b33bcfbb88505a7fc9630861924458f76d159276
unknown <tatu.riihimaki@gmail.com>
2020-01-27 16:47:08 +0200

    Added error component

---
+++ frontend/src/components/Error.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Error: {{error}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        error: String
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: #f75f5f;
+        padding: 20px 10px;
+        color: #fff;
+    }
+</style>
